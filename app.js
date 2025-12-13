459678c5d09e0a4a21830f8fce286d9427860440
maksymaf <forbidden344@gmail.com>
2025-12-13 10:40:53 +0000

    chore: add initial project structure

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/isalive', (req, res) => {
+    res.status(200).json({message: "it works"});    
+});
+
+async function main() {
+    try{
+        app.listen(PORT, () => {
+            console.log(`Server is listening on http://localhost:3000`);
+        })
+    }catch(error){
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+main();
